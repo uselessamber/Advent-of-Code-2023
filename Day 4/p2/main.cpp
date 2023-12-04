fbfbbb1b565034c44326246e5ca044abef2345fa
UselessAmber <amberthequeer@gmail.com>
2023-12-04 16:39:10 +1100

    Day 4 Completed (C++)

---
+++ Day 4/p2/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <time.h>
+#include <math.h>
+#include <iomanip>
+#define fileio(name) freopen(name".inp", "r", stdin); freopen(name".out", "w", stdout)
+#define len(v) (int)v.size()
+#define FS first
+#define SC second
+#define PB push_back
+#define LSOne(i) i & (-i)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+const bool open_file = true;
+const int N = 1e6 + 7;
+const int INF = 1e9 + 7;
+const double EPS = 1e-9;
+const ll INF64 = 1e16 + 7;
+int divceil(int x, int y){
+    return 1 + ((x - 1) / y);
+}
+//stuff to declare
+
+void tokenizer(string &inp, vector <string>&tokens, char splitter = ' '){
+    tokens.clear();
+    tokens.push_back("");
+    int top = 1;
+    for (char c : inp){
+        if (c == splitter){
+            if (tokens[top - 1].length() == 0) continue;
+            top += 1;
+            tokens.PB("");
+        }else{
+            tokens[top - 1] = tokens[top - 1] + c;
+        }
+    }
+}
+
+int str_to_int(string &s){
+    int val = 0;
+    for (char c : s){
+        val *= 10;
+        val += c - '0';
+    }
+    return val;
+}
+
+string text;
+set <int> winning_number;
+vi card_count(1000, 1);
+int game_count = 0;
+int answer = 0;
+
+int main(){
+    if (open_file){
+        fileio("file");
+    }
+    //main code
+    game_count += 1;
+    while (getline(cin, text)){
+        vector <string> game;
+        tokenizer(text, game);
+        int n = len(game);
+        bool hand_mode = false;
+        int found_win = 1;
+        for (int i = 2; i < n; i++){
+            if (game[i] == "|"){
+                hand_mode = true;
+                continue;
+            }
+            int curr = str_to_int(game[i]);
+            if (!hand_mode){
+                winning_number.insert(curr);
+            }else{
+                if (winning_number.find(curr) != winning_number.end()){
+                    card_count[game_count + found_win++]+= card_count[game_count];
+                }
+            }
+        }
+        answer += card_count[game_count];
+        winning_number.clear();
+        game_count += 1;
+    }
+    cout << answer;
+}
